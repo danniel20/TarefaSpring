1268415e739da1e0d4eb2a196a638912d592b82f
Caio Carvalho <caiocarvalho@great.ufc.br>
2014-04-28 11:37:19 -0300

    listener class for initialize database server

---
+++ src/main/java/br/com/tarefa/listener/DataBaseStartListener.java
@@ -0,0 +1,21 @@
+package br.com.tarefa.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.hsqldb.server.Server;
+import org.springframework.web.WebApplicationInitializer;
+
+public class DataBaseStartListener implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext arg0) throws ServletException {
+        Server hsqlServer = new Server();
+        hsqlServer.setDatabaseName(0, "tarefa");
+        hsqlServer.setDatabasePath(0, "file:target/data/tarefa");
+        hsqlServer.setPort(9001);
+        hsqlServer.setLogWriter(null);
+        hsqlServer.setSilent(true);
+        hsqlServer.start();
+    }
+}
